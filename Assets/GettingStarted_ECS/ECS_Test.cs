d01c7b57c6f8912c5890b9247b8bfb0587a7ea9b
WangChong <654306663@qq.com>
2020-06-12 17:01:20 +0800

    提交ECS测试、Jobs测试

---
+++ Assets/GettingStarted_ECS/ECS_Test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
+
+public class ECS_Test : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private Material material;
+
+    private void Start()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(IndexComponent),
+            typeof(Translation),
+            typeof(Scale),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(RenderBounds),
+            typeof(MoveSpeedComponent)
+            );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(1000, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+        for (int i = 0; i < entityArray.Length; i++)
+        {
+            Entity entity = entityArray[i];
+            entityManager.SetComponentData(entity, new IndexComponent { index = i });
+            entityManager.SetComponentData(entity, new MoveSpeedComponent { moveSpeed = 0.5f + i * 0.005f });
+            entityManager.SetComponentData(entity, new Translation { Value = new Unity.Mathematics.float3(Random.Range(-8, 8f), Random.Range(-5, 5f), 0) });
+            entityManager.SetComponentData(entity, new Scale { Value = 0.1f + i * 0.001f });
+
+            entityManager.SetSharedComponentData(entity, new RenderMesh { mesh = mesh, material = material });
+        }
+        entityArray.Dispose();
+    }
+}
---
+++ Assets/GettingStarted_Jobs/Jobs_Test2.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Collections;
+using UnityEngine.Jobs;
+
+public class Jobs_Test2 : MonoBehaviour
+{
+    [SerializeField] private bool useJobs1;
+    [SerializeField] private bool useJobs2;
+    [SerializeField] private Transform cubePrefab;
+    private List<Cube> cubeList;
+    public class Cube
+    {
+        public Transform transform;
+        public float moveY;
+    }
+
+    private void Start()
+    {
+        cubeList = new List<Cube>();
+        for (int i = 0; i < 1000; i++)
+        {
+            Transform cube = Instantiate(cubePrefab, new Vector3(UnityEngine.Random.Range(-8f, 8f), UnityEngine.Random.Range(-5f, 5f), 0), Quaternion.identity);
+            cubeList.Add(new Cube { transform = cube, moveY = UnityEngine.Random.Range(1f, 2f)});
+        }
+    }
+
+    private void Update()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        if (useJobs1)
+        {
+            NativeArray<float3> positionArray = new NativeArray<float3>(cubeList.Count, Allocator.TempJob);
+            NativeArray<float> moveYArray = new NativeArray<float>(cubeList.Count, Allocator.TempJob);
+
+            for (int i = 0; i < cubeList.Count; i++)
+            {
+                positionArray[i] = cubeList[i].transform.position;
+                moveYArray[i] = cubeList[i].moveY;
+            }
+
+            ReallyToughParallelJob reallyToughParallelJob = new ReallyToughParallelJob
+            {
+                deltaTime = Time.deltaTime,
+                positionArray = positionArray,
+                moveYArray = moveYArray
+            };
+
+            JobHandle jobHandle = reallyToughParallelJob.Schedule(cubeList.Count, 100);
+            jobHandle.Complete();
+
+            for (int i = 0; i < cubeList.Count; i++)
+            {
+                cubeList[i].transform.position = positionArray[i];
+                cubeList[i].moveY = moveYArray[i];
+            }
+            positionArray.Dispose();
+            moveYArray.Dispose();
+        }
+        else if (useJobs2)
+        {
+            TransformAccessArray transformAccessArray = new TransformAccessArray(cubeList.Count);
+            NativeArray<float> moveYArray = new NativeArray<float>(cubeList.Count, Allocator.TempJob);
+
+            for (int i = 0; i < cubeList.Count; i++)
+            {
+                transformAccessArray.Add(cubeList[i].transform);
+                moveYArray[i] = cubeList[i].moveY;
+            }
+            
+            ReallyToughParallelJobTransforms reallyToughParallelJobTransforms = new ReallyToughParallelJobTransforms
+            {
+                deltaTime = Time.deltaTime,
+                moveYArray = moveYArray
+            };
+            JobHandle jobHandle = reallyToughParallelJobTransforms.Schedule(transformAccessArray);
+            jobHandle.Complete();
+
+            for (int i = 0; i < cubeList.Count; i++)
+            {
+                cubeList[i].moveY = moveYArray[i];
+            }
+            transformAccessArray.Dispose();
+            moveYArray.Dispose();
+        }
+        else
+        {
+            foreach (Cube cube in cubeList)
+            {
+                cube.transform.position += new Vector3(0, cube.moveY * Time.deltaTime);
+                if (cube.transform.position.y > 5f)
+                {
+                    cube.moveY = -math.abs(cube.moveY);
+                }
+                if (cube.transform.position.y < -5f)
+                {
+                    cube.moveY = +math.abs(cube.moveY);
+                }
+
+                float value = 0f;
+                for (int i = 0; i < 1000; i++)
+                {
+                    value = math.exp10(math.sqrt(value));
+                }
+            }
+        }
+        Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
+    }
+}
+
+[BurstCompile]
+public struct ReallyToughParallelJob : IJobParallelFor
+{
+    public NativeArray<float3> positionArray;
+    public NativeArray<float> moveYArray;
+    public float deltaTime;
+
+    public void Execute(int index)
+    {
+        positionArray[index] += new float3(0, moveYArray[index] * deltaTime, 0);
+        if (positionArray[index].y > 5f)
+        {
+            moveYArray[index] = -math.abs(moveYArray[index]);
+        }
+        if (positionArray[index].y < -5f)
+        {
+            moveYArray[index] = +math.abs(moveYArray[index]);
+        }
+
+        float value = 0f;
+        for (int i = 0; i < 1000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
+
+[BurstCompile]
+public struct ReallyToughParallelJobTransforms : IJobParallelForTransform
+{
+    public NativeArray<float> moveYArray;
+    public float deltaTime;
+
+    public void Execute(int index, TransformAccess transform)
+    {
+        transform.localPosition += new Vector3(0, moveYArray[index] * deltaTime, 0);
+        if (transform.localPosition.y > 5f)
+        {
+            moveYArray[index] = -math.abs(moveYArray[index]);
+        }
+        if (transform.localPosition.y < -5f)
+        {
+            moveYArray[index] = +math.abs(moveYArray[index]);
+        }
+
+        float value = 0f;
+        for (int i = 0; i < 1000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
