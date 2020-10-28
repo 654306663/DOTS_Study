7dbf643f7fb37c5a58b65e903cd1b510537da056
WangChong <654306663@qq.com>
2020-10-28 22:56:14 +0800

    添加物理、射线、Trigger检测等

---
+++ Assets/03_Physics/BallJumpSystem.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using Unity.Physics;
+
+public class BallJumpSystem : ComponentSystem
+{
+    protected override void OnUpdate() {
+        Entities.ForEach((ref PhysicsVelocity physicsVelocity) =>{
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                physicsVelocity.Linear.y = 5f;
+            }
+        });
+    }
+}
---
+++ Assets/03_Physics/TestRaycast.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Mathematics;
+using Unity.Physics.Systems;
+
+public class TestRaycast : MonoBehaviour
+{
+    private Entity Raycast(float3 fromPosition, float3 toPosition)
+    {
+        PhysicsWorld physicsWorld = World.DefaultGameObjectInjectionWorld.GetExistingSystem<BuildPhysicsWorld>().PhysicsWorld;
+
+        RaycastInput raycastInput = new RaycastInput()
+        {
+            Start = fromPosition,
+            End = toPosition,
+            Filter = CollisionFilter.Default
+        };
+
+        Unity.Physics.RaycastHit raycastHit;
+        if (physicsWorld.CastRay(raycastInput, out raycastHit))
+        {
+            Entity hitEntity = raycastHit.Entity;
+            return hitEntity;
+        }
+        else
+        {
+            return Entity.Null;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance = 100f;
+            Debug.Log(Raycast(ray.origin, ray.direction * rayDistance));
+        }
+    }
+}
